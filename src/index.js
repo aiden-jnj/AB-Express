c84220db67c2a318cedf70c5e2c74afec534afe2
Aiden <bokjaeoh@gmail.com>
2022-01-03 18:08:42 +0900

    Add `express` dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const ABExpress = {}
+const { express, Router } = require('./server')
+
+
+const ABExpress = { express, Router }
 
 
 module.export = ABExpress
---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+
+const { Router } = express
+
+
+module.exports = { express, Router }
